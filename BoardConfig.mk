b138f5bbd692f5157ff564b31610e93949d4a0ad
ljzyal <ljzyal@gmail.com>
2013-11-14 14:09:28 +0800

    set default selinux
    Change-Id: I654d416e02464fd83a8ece6b26010d461c88be74

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_BOARD_INFO_FILE ?= device/samsung/superior/board-info.txt
 
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyO2,115200n8 mem=1024M androidboot.console=ttyO2 vram=20M omapfb.vram=0:16M
+BOARD_KERNEL_CMDLINE := console=ttyO2,115200n8 mem=1024M androidboot.console=ttyO2 vram=20M omapfb.vram=0:16M androidboot.selinux=permissive
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/piranha
