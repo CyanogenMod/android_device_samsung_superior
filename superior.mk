60b2892319bf6fbbc52962e2a220c9a4fb5a8b6b
ljzyal <ljzyal@gmail.com>
2015-07-20 13:26:38 +0800

    superior: Don't copy FFMPEG codec file
    Change-Id: I326a1f877cb0a5ee8910e0a56c76e247d18cc75b

--- superior.mk
+++ superior.mk
@@ -69,8 +69,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml
 
 # Packages
 PRODUCT_PACKAGES += \
